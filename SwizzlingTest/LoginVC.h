aa06b0fd150dc545bb8b05d20a01bc07e41635a4
Michalis Mavris <Mike@Michaliss-MacBook-Pro-2.local>
2017-07-05 00:48:52 +0300

    Added Swizzle project

---
+++ SwizzlingTest/LoginVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginVC.h
+//  SwizzlingTest
+//
+//  Created by Michalis Mavris on 24/12/2016.
+//  Copyright © 2016 Miksoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginVC : UIViewController
+
+@end
---
+++ SwizzlingTest/SwizzlingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwizzlingTest
+//
+//  Created by Michalis Mavris on 24/12/2016.
+//  Copyright © 2016 Miksoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
